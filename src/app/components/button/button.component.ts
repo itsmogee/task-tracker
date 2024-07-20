a075059557235170daba0e55169928f3b1ea35f6
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-20 16:01:56 +0200

    updated : button component
    added input for color and text for the button
    added output event emitted to transmit the event click
    style the button inline using ngStyle

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.css',
 })
 export class ButtonComponent {
+  // Inputs
+  @Input() color: string = '';
+  @Input() text: string = '';
 
+  // Outputs
+  @Output() btnClick = new EventEmitter();
+
+  onClick() {
+    this.btnClick.emit();
+  }
 }
