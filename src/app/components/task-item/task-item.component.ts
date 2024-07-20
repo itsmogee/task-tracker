d7d90b9fe9d5425585fd44c3d5b0de29c4e6ca8c
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-20 18:05:39 +0200

    update : task item
    styling of each task-item
    displaying the text and day in a div

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
   imports: [],
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.css'
+  styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
-
+  @Input() task: Task | undefined;
 }
