24f870d930a7b0c89e2918ae58f18b4d08171ed0
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-20 22:42:46 +0200

    updated : UI and styling
    title only shows up on main component
    grubox style colors added
    various stylings applied throughout

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -5,7 +5,7 @@ import { Observable, Subject } from 'rxjs';
   providedIn: 'root',
 })
 export class UiService {
-  private showAddTask: boolean = false;
+  private showAddTask?: boolean;
   private subject = new Subject<any>();
 
   constructor() {}
