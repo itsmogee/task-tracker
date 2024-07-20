e533360e88fb0e53e928581e994ce2199b10d259
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-20 16:27:05 +0200

    updated : task html to show each task item description

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-
+  tasks: Task[] = TASKS;
 }
