601f24bd5eedf326201f4dcd6acdbea8c80cd3a0
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-20 20:23:12 +0200

    added : add-task component with functionality
    submit functionality added
    Output eventEmitter to send to task-component module
    Form and inputs for each entry
    Styling for save task button

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.css'
+  styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter<Task>();
 
+  text = '';
+  day = '';
+  reminder = false;
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task');
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    // Clear inputs
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
