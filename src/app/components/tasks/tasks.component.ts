4f80066b1699607f27d042a09a7a6b52feece35b
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-20 18:43:27 +0200

    updated : tasks use Observables instead of directly using file
    Asynch functionality for getting data from TASKS

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
 import { NgFor } from '@angular/common';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -12,5 +12,11 @@ import { TaskItemComponent } from '../task-item/task-item.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  tasks: Task[] = TASKS;
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  tasks: Task[] = [];
 }
